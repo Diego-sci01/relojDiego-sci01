1ff4c38b8f8e441395825afe474565682b89a6da
Diego-sci01 <diegosantosantana2008@gmail.com>
2025-10-27 02:52:58 +0000

    Actualizacion

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,44 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+                  {/*Metodo del tutorial*/}
 
-//create your first component
-const Home = () => {
+function SecondsCounter(props) {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigCounter">
+			<div className="calendario">
+				<i className="fa-regular fa-clock"></i>
+			</div>
+			<div className="cuatro">{props.digitCuatro}</div>
+			<div className="tres">{props.digitTres}</div>
+			<div className="dos">{props.digitDos}</div>
+			<div className="uno">{props.digitUno}</div>
 		</div>
 	);
+}
+
+SecondsCounter.propTypes = {
+	digitCuatro: PropTypes.number,
+	digitTres: PropTypes.number,
+	digitDos: PropTypes.number,
+	digitUno: PropTypes.number
 };
 
-export default Home;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+let counter = 0;
+setInterval(() => {
+	const uno = Math.floor(counter / 1) % 10;
+	const dos = Math.floor(counter / 10) % 10;
+	const tres = Math.floor(counter / 100) % 10;
+	const cuatro = Math.floor(counter / 1000) % 10;
+
+	root.render(
+		<SecondsCounter digitUno={uno} digitDos={dos} digitTres={tres} digitCuatro={cuatro}/>
+	);
+
+	counter++;
+}, 1000);
+
+export default SecondsCounter;
